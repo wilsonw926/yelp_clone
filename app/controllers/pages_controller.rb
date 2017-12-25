a88142261d9b594c2f50c7535d2629303920cbc6
Wilson Wang <wilsonw926@gmail.com>
2017-12-25 15:55:40 -0800

    about us and contact us page plus pg for heroku

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+
+	def about_us
+	end
+
+	def contact_us
+	end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
 	root 'restaurants#index'
+	match '/about_us', to: 'pages#about_us', via: :get
+	match '/contact_us', to: 'pages#contact_us', via: :get
 end
