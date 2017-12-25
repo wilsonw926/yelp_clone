c39353b0e2159c23f23209a35beb03d778aa96a0
Wilson Wang <wilsonw926@gmail.com>
2017-12-24 23:34:41 -0800

    template

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,10 @@
+//placeholder
+$(document).ready(function() { 
+	$('input, textarea').placeholder();
+});
+//Homepage Carousel
+$(document).ready(function(){
+	$('#realto-carousel').carousel({
+		interval: 5000
+	});
+});
