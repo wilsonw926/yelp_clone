a109fef32b2bca9aea22c007e0aa9a9ce7933b93
Wilson Wang <wilsonw926@gmail.com>
2017-12-27 14:04:12 -0800

    customization with homepage, added background pickture and font styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  
   devise_for :users
-	root 'restaurants#index'
-	match '/about_us', to: 'pages#about_us', via: :get
-	match '/contact_us', to: 'pages#contact_us', via: :get
+  resources :restaurants
+  root 'restaurants#index'
+  
+  match '/about_us', to: 'pages#about_us', via: :get
+  match '/contact_us', to: 'pages#contact_us', via: :get
 end
