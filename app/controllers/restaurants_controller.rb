2660320d460135d909ad580560b42ac809bb7739
Wilson Wang <wilsonw926@gmail.com>
2017-12-27 14:57:05 -0800

    create restaurant form

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,4 +4,25 @@ class RestaurantsController < ApplicationController
 		
 	end
 
+	def new
+		@restaurant = Restaurant.new
+	end
+
+	def create
+		@restaurant = Restaurant.new
+		@restaurant.update(restaurant_params)
+		@restaurant.save
+ 		redirect_to @restaurant
+	end
+
+	def show
+
+	end
+
+	private
+		def restaurant_params
+			params.require(:restaurant).permit(:name, :description, :address1, :address2,
+											   :city, :state, :zipcode, :phone, :email)
+		end
+
 end
