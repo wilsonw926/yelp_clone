2067a2dabf463b95331d6f9ae602b331faa9f854
Wilson Wang <wilsonw926@gmail.com>
2017-12-28 15:55:15 -0800

    resetting database

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,26 +1,32 @@
 class RestaurantsController < ApplicationController
-  	
-	def index
-		
-	end
+  
+  def index
+    @restaurant = Restaurant.all
+  end
 
-	def new
-		@restaurant = Restaurant.new
-	end
+  def new
+    @restaurant = Restaurant.new
+  end
 
-	def create
-		@restaurant = Restaurant.create(restaurant_params)
- 		redirect_to @restaurant
-	end
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    if @restaurant.save
+      flash['success'] = 'Review has been created succesfully.'
+      redirect_to @restaurant
+    else
+      flash.now['danger'] = 'There was a problem with your review.'
+      render 'new'
+    end
+  end
 
-	def show
-		
-	end
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
 
-	private
-		def restaurant_params
-			params.require(:restaurant).permit(:name, :description, :address1, :address2,
-											   :city, :state, :zipcode, :phone, :email)
-		end
+  private
+    def restaurant_params
+      params.require(:restaurants).permit(:name, :description, :address1, :address2,
+                         :city, :state, :zipcode, :phone, :email)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :restaurants
   root to: 'restaurants#index'
   
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
+
   match '/about_us', to: 'pages#about_us', via: :get
   match '/contact_us', to: 'pages#contact_us', via: :get
 end
