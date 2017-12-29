c541a87c7eb083dd9ac29b63d4caf055651ecf4b
Wilson Wang <wilsonw926@gmail.com>
2017-12-29 01:10:19 -0800

    add first last name for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :add_parameters_to_user, if: :devise_controller?
+
+  def add_parameters_to_user
+  	devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+  	devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
+	devise_parameter_sanitizer.permit(:account_update, keys: [:first_name])
+  	devise_parameter_sanitizer.permit(:account_update, keys: [:last_name])
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171229064656) do
+ActiveRecord::Schema.define(version: 20171229083901) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 20171229064656) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
