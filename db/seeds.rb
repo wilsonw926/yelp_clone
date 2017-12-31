4780040e91250e71065f0b7f2528d3b0471903d3
Wilson Wang <wilsonw926@gmail.com>
2017-12-30 20:34:30 -0800

    populate seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#Food Categories
+american = Category.where(name: "American").first_or_create(name: "American")
+asian = Category.where(name: "Asian").first_or_create(name: "Asian")
+french = Category.where(name: "French").first_or_create(name: "French")
+italian = Category.where(name: "Italian").first_or_create(name: "Italian")
+mediterranean = Category.where(name: "Mediterranean").first_or_create(name: "Mediterranean")
+mexican = Category.where(name: "Mexican").first_or_create(name: "Mexican")
 
-Category.where(name: "American").first_or_create(name: "American")
-Category.where(name: "Asian").first_or_create(name: "Asian")
-Category.where(name: "French").first_or_create(name: "French")
-Category.where(name: "Greek").first_or_create(name: "Greek")
-Category.where(name: "Italian").first_or_create(name: "Italian")
-Category.where(name: "Mediterranean").first_or_create(name: "Mediterranean")
-Category.where(name: "Mexican").first_or_create(name: "Mexican")
+#Restaurants Ex: 
+#Restaurant.where(name: "Burrita").first_or_create(name: "Burrita", description: "asdf", address1: "Ave", city: "Berkeley", state: "CA", zipcode: "94704", phone: "4859", category_id: mexican.id, image: File.new("app/assets/images/Laburrita.jpg"))
+
+#American
+Restaurant.where(name: "Eureka!").first_or_create(name: "Eureka!", description: "Elevated menu of burgers, mains, local brews & whiskey in stylish, bustling environs.", address1: "2068 Center St", city: "Berkeley", state: "CA", zipcode: "94704", phone: "(510) 809-8282", category_id: american.id, image: File.new("app/assets/images/eureka.jpg"))
+Restaurant.where(name: "Snack Shack").first_or_create(name: "Snack Shack", description: "The Snack Shack is a fast casual restaurant in Berkeley, California offering gourmet burgers, snacks and craft beers.", address1: "2505 Hearst Ave", city: "Berkeley", state: "CA", zipcode: "94709", phone: "(510) 679-5171", category_id: american.id, image: File.new("app/assets/images/snackshack.jpg"))
+Restaurant.where(name: "McDonald's").first_or_create(name: "McDonald's", description: "Classic, long-running fast-food chain known for its burgers, fries & shakes.", address1: "1998 Shattuck Ave", city: "Berkeley", state: "CA", zipcode: "94704", phone: "(510) 843-1500", category_id: american.id, image: File.new("app/assets/images/mcdonalds.jpg"))
+Restaurant.where(name: "Venus Restaurant").first_or_create(name: "Venus Restaurant", description: "Californian eatery featuring seasonal dishes & organic wine in a bustling brick-walled space.", address1: "2327 Shattuck Ave", city: "Berkeley", state: "CA", zipcode: "94704", phone: "(510) 540-5950", category_id: american.id, image: File.new("app/assets/images/venus.jpg"))
+
+#Asian
+Restaurant.where(name: "Thai Basil").first_or_create(name: "Thai Basil", description: "A nice option for Thai food, famous among Berkeley students as a perfect spot for late-night eats.", address1: "2519 Durant Ave F", city: "Berkeley", state: "CA", zipcode: "94704", phone: "(510) 548-6692", category_id: asian.id, image: File.new("app/assets/images/thaibasil.jpg"))
+Restaurant.where(name: "Amakara").first_or_create(name: "Amakara", description: "House sushi rolls join noodles, tempura & other Japanese dishes in this spacious, modern restaurant.", address1: "7222 Regional St", city: "Dublin", state: "CA", zipcode: "94568", phone: "(925) 803-8485", category_id: asian.id, image: File.new("app/assets/images/amakara.jpg"))
+Restaurant.where(name: "Toss Noodle Bar").first_or_create(name: "Toss Noodle Bar", description: "Compact storefront eatery with modern decor serving up noodle entrees & soup, plus rice dishes.", address1: "2272 Shattuck Ave", city: "Berkeley", state: "CA", zipcode: "94704", phone: "(510) 845-8677", category_id: asian.id, image: File.new("app/assets/images/toss.jpg"))
+Restaurant.where(name: "KoJa Kitchen").first_or_create(name: "KoJa Kitchen", description: "Food truck team's stylish permanent space serving innovative Korean & Japanese fusion fare.", address1: "2395 Telegraph Ave", city: "Berkeley", state: "CA", zipcode: "94704", phone: "(510) 962-5652", category_id: asian.id, image: File.new("app/assets/images/koja.jpg"))
+
+#French
+Restaurant.where(name: "La Note").first_or_create(name: "La Note", description: "Lively setting for French eats for breakfast, lunch or dinner, plus a garden open in good weather.", address1: "2377 Shattuck Ave", city: "Berkeley", state: "CA", zipcode: "94704", phone: "(510) 843-1525", category_id: french.id, image: File.new("app/assets/images/Lanote.jpg"))
+Restaurant.where(name: "Chez Panisse").first_or_create(name: "Chez Panisse", description: "Alice Waters' acclaimed flagship downstairs & cafe upstairs offer seasonal fare & a chic, cozy vibe.", address1: "1517 Shattuck Ave, Berkeley", city: "Berkeley", state: "CA", zipcode: "94709", phone: "(510) 548-5525", category_id: french.id, image: File.new("app/assets/images/chezpanisse.jpg"))
+Restaurant.where(name: "Gregoire").first_or_create(name: "Gregoire", description: "Neat eatery dispenses signature potato puffs & other gourmet French fast food, mostly for takeout.", address1: "2109 Cedar St", city: "Berkeley", state: "CA", zipcode: "94709", phone: "(510) 883-1893", category_id: french.id, image: File.new("app/assets/images/Gregoire.jpg"))
+
+#Italian
+Restaurant.where(name: "Cheese Board Pizza").first_or_create(name: "Cheese Board Pizza", description: "A pizzeria, bakery & cheese shop, this spot serves 1 style of veggie pizza per day & limited seats.", address1: "1512 Shattuck Ave", city: "Berkeley", state: "CA", zipcode: "94709", phone: "(510) 549-3183", category_id: italian.id, image: File.new("app/assets/images/cheeseboard.jpg"))
+Restaurant.where(name: "Gypsy's Trattoria Italiana").first_or_create(name: "Gypsy's Trattoria Italiana", description: "Fast-food outfit with late hours provides Italian comfort dishes to a student-heavy clientele.", address1: "2519 Durant Ave", city: "Berkeley", state: "CA", zipcode: "94704", phone: "(510) 548-4860", category_id: italian.id, image: File.new("app/assets/images/gypsys.png"))
+Restaurant.where(name: "Lo Coco's").first_or_create(name: "Lo Coco's", description: "Pizza & homemade pasta made from Sicilian recipes, served amid traditional Italian restaurant decor.", address1: "1400 Shattuck Ave", city: "Berkeley", state: "CA", zipcode: "94709", phone: "(510) 843-3745", category_id: italian.id, image: File.new("app/assets/images/lococo.jpg"))
+Restaurant.where(name: "Pasta Bene").first_or_create(name: "Pasta Bene", description: "Thin-crust pizzas, happy hour & classic Italian fare in a bright, airy eatery with a patio.", address1: "2565 Telegraph Ave", city: "Berkeley", state: "CA", zipcode: "94704", phone: "(510) 845-2363", category_id: italian.id, image: File.new("app/assets/images/pastabene.jpg"))
+
+#Mediterranean
+Restaurant.where(name: "La Mediterranee").first_or_create(name: "La Mediterranee", description: "Longtime Middle Eastern-Mediterranean spot known for its meze sampler platters & outdoor seating.", address1: "2936 College Ave", city: "Berkeley", state: "CA", zipcode: "94705", phone: "(510) 540-7773", category_id: mediterranean.id, image: File.new("app/assets/images/Lamediterranee.jpg"))
+Restaurant.where(name: "Oasis Grill").first_or_create(name: "Oasis Grill", description: "Narrow counter-serve destination features Mediterranean comfort eats prepared in an open kitchen.", address1: "2114 Center St", city: "Berkeley", state: "CA", zipcode: "94704", phone: "(510) 666-8951", category_id: mediterranean.id, image: File.new("app/assets/images/oasisgrill.jpg"))
+Restaurant.where(name: "Zut!").first_or_create(name: "Zut!", description: "Mediterranean fare from local, organic ingredients, plus handcrafted cocktails, in a sunny space.", address1: "1820 Fourth St", city: "Berkeley", state: "CA", zipcode: "94710", phone: "(510) 644-0444", category_id: mediterranean.id, image: File.new("app/assets/images/zut.jpg"))
+
+#Mexican
+Restaurant.where(name: "La Burrita").first_or_create(name: "La Burrita", description: "Burritos & other Mexican classics star at this snug spot with a few tables & outdoor seats.", address1: "2530 Durant Ave", city: "Berkeley", state: "CA", zipcode: "94704", phone: "(510) 845-4859", category_id: mexican.id, image: File.new("app/assets/images/Laburrita.jpg"))
+Restaurant.where(name: "Taco Bell").first_or_create(name: "Taco Bell", description: "Fast-food chain serving Mexican-inspired fare such as tacos, quesadillas & nachos.", address1: "2528 Durant Ave", city: "Berkeley", state: "CA", zipcode: "94704", phone: "(510) 984-0079", category_id: mexican.id, image: File.new("app/assets/images/tacobell.jpg"))
+Restaurant.where(name: "Comal").first_or_create(name: "Comal", description: "Modern design & Mexican cuisine meet at this eatery known for its cocktails & vibrant outdoor area.", address1: "2020 Shattuck Ave", city: "Berkeley", state: "CA", zipcode: "94704", phone: "(510) 926-6300", category_id: mexican.id, image: File.new("app/assets/images/comal.jpg"))
+Restaurant.where(name: "Tacos Sinaloa").first_or_create(name: "Tacos Sinaloa", description: "Compact, bright & playfully outfitted taco shop with Mexican basics, veggie options & aguas frescas.", address1: "2384 Telegraph Ave", city: "Berkeley", state: "CA", zipcode: "94704", phone: "(510) 665-7895", category_id: mexican.id, image: File.new("app/assets/images/tacosinaloa.jpg"))
