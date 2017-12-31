5d44ea9daa797ca8100acc33b108780ad5baf321
Wilson Wang <wilsonw926@gmail.com>
2017-12-31 11:14:38 -0800

    added visitor lat and long so results show restaurants near user

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,18 @@
 class RestaurantsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :new]
+  
   def index
-    @restaurants = Restaurant.all
-    @italian_restaurants = Restaurant.where(category_id: 4)
-    @asian_restaurants = Restaurant.where(category_id: 2)
-    @american_restaurants = Restaurant.where(category_id: 1)
+    #Testing Purposes
+    visitor_latitude = 37.875356
+    visitor_longitude = -122.265755
+
+    #visitor_latitude = request.location.latitude
+    #visitor_longitude = request.location.longitude
+
+    @restaurants = Restaurant.near([visitor_latitude, visitor_longitude], 20)
+    @italian_restaurants = Restaurant.where(category_id: 4).near([visitor_latitude, visitor_longitude], 20)
+    @asian_restaurants = Restaurant.where(category_id: 2).near([visitor_latitude, visitor_longitude], 20)
+    @american_restaurants = Restaurant.where(category_id: 1).near([visitor_latitude, visitor_longitude], 20)
   end
 
   def new
